d570a274957872b9735cc18b07d5ee905b193d80
psyas8 <psyas8@exmail.nottingham.ac.uk>
2020-04-08 17:50:08 +0100

    Added upload project view components Successfully adding PDF to application ready for upload

--- app/src/main/java/com/example/ideation/activities/NewProjectActivity.java
+++ app/src/main/java/com/example/ideation/activities/NewProjectActivity.java
@@ -3,10 +3,17 @@ package com.example.ideation.activities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.ideation.database.IdeationContract;
@@ -18,7 +25,9 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
@@ -26,27 +35,43 @@ import java.util.Map;
 
 public class NewProjectActivity extends AppCompatActivity {
 	private static final String TAG = "NewProjectActivity";
+	private static final int PICK_PDF_REQUEST = 1;
 
 	//Initialise variables
 	EditText titleField, descriptionField, categoryField;
 	String titleText, descriptionText, categoryText;
-	private FirebaseAuth firebaseAuth;
+	private Button buttonChoosePdf;
+	private Button buttonUpload;
+	private TextView textViewShowUpload;
+	private EditText pdfFileName;
+	private ProgressBar progressBar;
+
+	//Declare a URI for the PDF
+	private Uri pdfUri;
 
 	//Make an database instance
+	private FirebaseAuth firebaseAuth;
 	private FirebaseFirestore db = FirebaseFirestore.getInstance();
+	private FirebaseStorage storage = FirebaseStorage.getInstance();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_project);
 
-		// Initialize Firebase Auth
+		//Initialize Firebase Auth
 		firebaseAuth = FirebaseAuth.getInstance();
 
 		//Assign views to variables
 		titleField = findViewById(R.id.projectTitle);
 		descriptionField = findViewById(R.id.projectDescription);
 		categoryField = findViewById(R.id.projectCategory);
+		//Upload Views
+		buttonChoosePdf = findViewById(R.id.button_choose_image);
+		buttonUpload = findViewById(R.id.button_upload);
+		textViewShowUpload = findViewById(R.id.text_view_show_upload);
+		pdfFileName = findViewById(R.id.edit_text_file_name);
+		progressBar = findViewById(R.id.progress_bar);
 	}
 
 	public void onAddProject(View v) {
@@ -110,5 +135,35 @@ public class NewProjectActivity extends AppCompatActivity {
 						Log.d(TAG, e.toString());
 					}
 				});
+	}
+
+	public void onChoosePdf (View v) {
+		Intent intent = new Intent();
+		intent.setType("application/pdf");
+		intent.setAction(Intent.ACTION_GET_CONTENT);
+		startActivityForResult(intent, PICK_PDF_REQUEST);
+	}
+
+	private void openFileChooser() {
+
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		if (requestCode == PICK_PDF_REQUEST && resultCode == RESULT_OK
+				&& data != null && data.getData() != null) {
+			pdfUri = data.getData();
+
+			//Create a cursor to retrieve the name and get the name index
+			Cursor cursor = getContentResolver().query(pdfUri, null, null, null, null);
+			int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+			cursor.moveToFirst();
+
+			//Set the upload text to the PDF file name and close the cursor
+			textViewShowUpload.setText(cursor.getString(nameIndex));
+			cursor.close();
+		}
 	}
 }
